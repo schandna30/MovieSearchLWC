e6b9c26b286c615a13a0c231081d163ecbcaf342
Simran <schandna308@gmail.com>
2025-08-02 22:10:38 +0530

    MovieSearch

---
+++ MovieSearchLWC/force-app/main/default/lwc/movieSearch/movieSearch.js
@@ -0,0 +1,50 @@
+import { LightningElement } from 'lwc';
+const DELAY=400;
+
+export default class MovieSearch extends LightningElement {
+
+    selectedType='';
+    loading=false;
+    selectedSearch='';
+    selectedPage="1";
+    delayTimeout;
+    get typeOptions(){
+        return [
+            {label:'None', value:'' },
+            {label:'Movie', value:'movie' },
+            {label:'Series', value:'series' },
+            {label:'Episode', value:'episode' },
+
+        ];
+    }
+    handleTypeChange(event){
+        let {name,value}=event.target;
+        this.loading=true;
+        if(name==="type"){
+            this.selectedType=value;
+        }
+        else if(name==="search"){
+            this.selectedSearch=value;
+        }
+        else if(name=="pagenumber")
+        {
+            this.selectedPage=value;
+        }
+        //debouncing
+        clearTimeout( this.delayTimeout);
+       this.delayTimeout= setTimeout(()=>{this.searchMovie();},DELAY)
+        
+
+    }
+    //this method will search for the entered movie
+
+     async searchMovie(){
+        const url=`https://www.omdbapi.com/?s=${this.selectedSearch}&type=${this.selectedType}&page=${this.selectedPage}&apikey=c3875512`;
+        const res= await fetch(url);
+        const data=await res.json();
+        console.log('Output of search',data);
+        this.loading=false;
+        
+    }
+
+}
